4e0fbff3b82db9d3cbf326a22ae3514bb9787620
kMlad <kmlad98@gmail.com>
2023-11-22 19:56:01 +0100

    Finish variables lesson

---
+++ 01-fundamentals/variables.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	const DaysInYear = 365
+
+	var favoriteColor string = "black"
+	fmt.Println("My favorite color is", favoriteColor)
+
+	var birthYear, age = 1998, 25
+	fmt.Println("I'm born in ", birthYear, " and I'm ", age, " years old.")
+
+	var (
+		firstInitial = "K"
+		lastInitial  = "M"
+	)
+	fmt.Println("I'm ", firstInitial, ".", lastInitial, ".")
+
+	var ageInDays int
+	ageInDays = age * DaysInYear
+	fmt.Println("I am ", ageInDays, " days old.")
+
+}
